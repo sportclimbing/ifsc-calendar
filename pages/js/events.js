de6ac0936948163fa3ed219051fc2d277877edcd
Nico <nicoswd@pm.me>
2023-05-06 01:03:46 +0200

    Fix scroll issue on page reload

--- pages/js/events.js
+++ pages/js/events.js
@@ -69,6 +69,23 @@ function element_is_in_viewport (el) {
     );
 }
 
+function remove_hash() {
+    let scrollV, scrollH, loc = window.location;
+    if ("pushState" in history)
+        history.pushState("", document.title, loc.pathname + loc.search);
+    else {
+        // Prevent scrolling by storing the page's current scroll offset
+        scrollV = document.body.scrollTop;
+        scrollH = document.body.scrollLeft;
+
+        loc.hash = "";
+
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scrollV;
+        document.body.scrollLeft = scrollH;
+    }
+}
+
 const refresh = (async () => {
     const response = await fetch("events/events.json");
     const jsonData = await response.json();
@@ -181,7 +198,8 @@ const refresh = (async () => {
     });
 
     if (!element_is_in_viewport(leagueElement)) {
-        window.location = `#event-${selectedLeague}`;
+        leagueElement.scrollIntoView();
+        remove_hash();
     }
 });
 
