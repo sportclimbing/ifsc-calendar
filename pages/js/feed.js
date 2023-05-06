aac472bdbe307a2222f8fd59707ee7cb323f76cc
Nico <nicoswd@pm.me>
2023-05-06 11:55:37 +0200

    Improve RSS feed

--- pages/js/feed.js
+++ pages/js/feed.js
@@ -1,25 +1,28 @@
-const RSS_URL = `feed/rss.xml`;
-const container = document.getElementById("container");
-const template = document.getElementById("news-item-template");
+(() => {
+    let RSS_URL = `feed/rss.xml`;
+    let container = document.getElementById("container");
+    let template = document.getElementById("news-item-template");
 
-function html(el, selector) {
-    let e = el.querySelector(selector);
-    return e ? e.innerHTML : null;
-}
+    function html(el, selector) {
+        let e = el.querySelector(selector);
+        return e ? e.innerHTML : null;
+    }
 
-fetch(RSS_URL)
-    .then(response => response.text())
-    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-    .then((data) => {
-        data.querySelectorAll("item").forEach(el => {
-            try {
-                const clone = template.content.cloneNode(true);
-                clone.getElementById('ifsc-title').innerText = html(el, "title");
-                clone.getElementById('ifsc-title').href = html(el, "guid");
-                clone.getElementById('ifsc-image').src = html(el, "description").match(/src="([^"]+)"/)[1];
-                clone.getElementById('ifsc-date').innerText = new Date(html(el, "pubDate")).toLocaleString();
+    fetch(RSS_URL)
+        .then(response => response.text())
+        .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+        .then((data) => {
+            data.querySelectorAll("item").forEach(el => {
+                try {
+                    let clone = template.content.cloneNode(true);
+                    clone.getElementById('ifsc-title').innerHTML = html(el, "title");
+                    clone.getElementById('ifsc-title').href = html(el, "guid");
+                    clone.getElementById('ifsc-image').src = html(el, "description").match(/src="([^"]+)"/)[1];
+                    clone.getElementById('ifsc-date').innerText += dayjs(html(el, "pubDate")).fromNow();
 
-                container.appendChild(clone);
-            } catch (e) {}
-        })
-    });
+                    container.appendChild(clone);
+                } catch (e) {
+                }
+            })
+        });
+})();
