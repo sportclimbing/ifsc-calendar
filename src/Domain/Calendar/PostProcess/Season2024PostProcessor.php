861ca71413f637f1bf4fb5393c82dd9ae4b8297d
Nico Oelgart <nicoswd@gmail.com>
2024-05-18 12:13:35 +0200

    Hardcode OQS schedule

--- src/Domain/Calendar/PostProcess/Season2024PostProcessor.php
+++ src/Domain/Calendar/PostProcess/Season2024PostProcessor.php
@@ -7,13 +7,15 @@
  */
 namespace nicoSWD\IfscCalendar\Domain\Calendar\PostProcess;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use Exception;
 use nicoSWD\IfscCalendar\Domain\Event\IFSCEvent;
 use nicoSWD\IfscCalendar\Domain\Event\Info\IFSCEventInfo;
+use nicoSWD\IfscCalendar\Domain\Round\IFSCRound;
 use nicoSWD\IfscCalendar\Domain\Round\IFSCRoundFactory;
-use nicoSWD\IfscCalendar\Domain\Round\IFSCRoundProviderInterface;
 use nicoSWD\IfscCalendar\Domain\Round\IFSCRoundStatus;
-use nicoSWD\IfscCalendar\Domain\Schedule\IFSCSchedule;
+use nicoSWD\IfscCalendar\Domain\Schedule\IFSCScheduleFactory;
 use nicoSWD\IfscCalendar\Domain\Stream\LiveStream;
 
 final readonly class Season2024PostProcessor
@@ -21,12 +23,11 @@ final readonly class Season2024PostProcessor
     private const int WUJIANG_IFSC_EVENT_ID = 1354;
     private const string WUJIAN_POSTER_URL = 'https://res.dasheng.top/227/pro/ctms_tool/20240323/jpg/d7f6d496a71d4dc785c8d0e51169d17b.jpg';
     private const int OLYMPIC_QUALIFIERS_SHANGHAI_ID = 1384;
-    private const string OLYMPIC_QUALIFIERS_SHANGHAI_INFO_SHEET = 'https://images.ifsc-climbing.org/ifsc/image/private/t_q_good/prd/yg3cqznmay12orsv9hpa.pdf';
     private const string OLYMPIC_QUALIFIERS_SHANGHAI_LIVE_STREAM = 'https://olympics.com/en/sport-events/olympic-qualifier-series-2024-shanghai/broadcasting-schedule';
 
     public function __construct(
-        private IFSCRoundProviderInterface $roundProvider,
         private IFSCRoundFactory $roundFactory,
+        private IFSCScheduleFactory $scheduleFactory,
     ) {
     }
     /**
@@ -59,25 +60,47 @@ final readonly class Season2024PostProcessor
 
     private function fetchOlympicQualifiersShanghaiRounds(IFSCEvent $event): array
     {
-        $rounds = [];
         $eventInfo = IFSCEventInfo::fromEvent($event);
-        /** @var IFSCSchedule[] $schedules */
-        $schedules = $this->roundProvider->fetchRoundsFromInfoSheet(
-            event: $eventInfo,
-            infoSheetUrl: self::OLYMPIC_QUALIFIERS_SHANGHAI_INFO_SHEET,
+
+        return [
+            // 16/5
+            $this->round($eventInfo, "Men's & Women's Boulder Qualification", '2024-05-16T04:30:00+02:00'),
+
+            // 17/5
+            $this->round($eventInfo, "Men's & Women's Lead Qualification", '2024-05-17T04:00:00+02:00'),
+            $this->round($eventInfo, "Women's Speed Qualification", '2024-05-17T10:50:00+02:00'),
+            $this->round($eventInfo, "Men's Speed Qualification", '2024-05-17T11:45:00+02:00'),
+
+            // 18/5
+            $this->round($eventInfo, "Men's & Women's Boulder Semifinal", '2024-05-18T03:30:00+02:00'),
+            $this->round($eventInfo, "Men's & Women's Lead Semifinal", '2024-05-18T07:30:00+02:00'),
+            $this->round($eventInfo, "Men's & Women's Speed Final", '2024-05-18T11:00:00+02:00'),
+
+            // 19/05
+            $this->round($eventInfo, "Men's Boulder Final", '2024-05-19T04:00:00+02:00'),
+            $this->round($eventInfo, "Men's Lead Final", '2024-05-19T06:05:00+02:00'),
+            $this->round($eventInfo, "Women's Boulder Final", '2024-05-19T09:25:00+02:00'),
+            $this->round($eventInfo, "Women's Lead Final", '2024-05-19T11:30:00+02:00'),
+        ];
+    }
+
+    private function round(IFSCEventInfo $eventInfo, string $title, string $startsAt): IFSCRound
+    {
+        $schedule = $this->scheduleFactory->create(
+            name: $title,
+            startsAt: (new DateTimeImmutable($startsAt))->setTimezone(new DateTimeZone('Europe/Madrid')),
+            endsAt: null,
         );
 
-        foreach ($schedules as $schedule) {
-            $rounds[] = $this->roundFactory->create(
-                event: $eventInfo,
-                roundName: $schedule->name,
-                startTime: $schedule->startsAt,
-                endTime: $schedule->endsAt,
-                status: IFSCRoundStatus::PROVISIONAL,
-                liveStream: new LiveStream(self::OLYMPIC_QUALIFIERS_SHANGHAI_LIVE_STREAM),
-            );
-        }
+        $liveStream = new LiveStream(self::OLYMPIC_QUALIFIERS_SHANGHAI_LIVE_STREAM);
 
-        return $rounds;
+        return $this->roundFactory->create(
+            event: $eventInfo,
+            roundName: $schedule->name,
+            startTime: $schedule->startsAt,
+            endTime: $schedule->endsAt,
+            status: IFSCRoundStatus::CONFIRMED,
+            liveStream: $liveStream,
+        );
     }
 }
